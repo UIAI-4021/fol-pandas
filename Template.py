75fa8c076164ec0f6b725430339b5829b24ed311
Kianoosh <kia.vadaei@gmail.com>
2024-01-21 16:39:17 +0330

    untill step 4

--- Template.py
+++ Template.py
@@ -67,27 +67,72 @@ class App(tkinter.Tk):
         self.marker_list = []  # Keeping track of markers
 
     def check_connections(self, results):
+        adjacency_df = pd.read_csv('Adjacency_matrix.csv')
         print('result2 ', results)
         locations = []
+
+        for _,row in adjacency_df.iterrows():
+            for i in range(len(row)):
+                if row[i] == 1:
+                    prolog.assertz(f"directly_connected('{str(row[0]).lower()}', \"{str(adjacency_df.columns[i]).lower()}\")")
+        
+        prolog.assertz("connected(X, Y) :- directly_connected(X, Y)")
+        prolog.assertz("connected(X, Y) :- directly_connected(Y, X)")
+
+        prolog.assertz("connected(X, Y) :- directly_connected(X, Z), connected(Z, Y)")
+
+        def search(queary):
+            connected_cities = set()
+            connected = list(prolog.query(queary))
+            for dest in connected:
+                connected_city = dest['X']
+                if isinstance(connected_city, bytes):
+                    connected_city = connected_city.decode('utf-8')
+                print(connected_city)
+                connected_cities.add(connected_city)
+            return connected_cities
+        connected_dict = {}
         for result in results:
             city  = result["City"]
             locations.append(city)
             # TODO 5: create the knowledgebase of the city and its connected destinations using Adjacency_matrix.csv
+            print(city)
+            query = f"connected('{city}', X)"
+            connected_dict[city] =  search(query)
+    
 
-
+        
         return locations
 
     def process_text(self):
         """Extract locations from the text area and mark them on the map."""
         text = self.text_area.get("1.0", "end-1c")  # Get text from text area
-        locations = self.extract_locations(text)  # Extract locations (you may use a more complex method here)
+        features_dict = self.extract_locations(text)
+        # locations = features_dict['']  # Extract locations (you may use a more complex method here)
+        query_dict = {}
+        
+        temp_keys = ['A', 'B', 'C' , 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L']
+        tmp_i = 0 
+
+        for feature_set in features_dict:
+            str = '('
+            for feature in features_dict[feature_set]:
+                if feature == '_':
+                    str += temp_keys[tmp_i] + ' = '  + feature + ' ; '
+                else:
+                    str += temp_keys[tmp_i] + ' = ' + "'" + feature + "'" + ' ; '
+            str = str[:-3]
+            str += ')'
+            query_dict[temp_keys[tmp_i]] = str
+            tmp_i += 1
+        query = f"destination(City,A, B, C, D, E, F, G, H, I, J, K, L), {query_dict['A']}, {query_dict['B']}, {query_dict['C']}, {query_dict['D']}, {query_dict['E']}, {query_dict['F']}, {query_dict['G']}, {query_dict['H']}, {query_dict['I']}, {query_dict['J']}, {query_dict['K']}, {query_dict['L']}"
 
 
         # TODO 4: create the query based on the extracted features of user desciption 
         ################################################################################################
-        query = "destination(City,_, _, _, low, _, _, _, _, _, _, _, _)"
+        # query = "destination(City,_, _, _, low, _, _, _, _, _, _, _, _)"
         results = list(prolog.query(query))
-        print(results)
+        locations = []
         locations = self.check_connections(results)
         # TODO 6: if the number of destinations is less than 6 mark and connect them 
         ################################################################################################
@@ -124,15 +169,15 @@ class App(tkinter.Tk):
         # TODO 3: extract key features from user's description of destinations
         ################################################################################################
         entered_features = {}
-        words = set(chain.from_iterable(line.strip().lower().split(' ') for line in text.split('\n') if line.strip()))
+        words = {str(item).replace('-', ' ') for item in set(chain.from_iterable(line.strip().lower().split(' ') for line in text.split('\n') if line.strip()))}
         for feature_key in uniqe_features_dict:
             intersection = words.intersection(uniqe_features_dict[feature_key])
             if intersection:
                 entered_features[feature_key] = intersection
             else:
-                entered_features[feature_key] = '-'
+                entered_features[feature_key] = '_'
         
-        return [line.strip() for line in text.split('\n') if line.strip()]
+        return entered_features
 
     def start(self):
         self.mainloop()
@@ -145,6 +190,9 @@ prolog.retractall("destination(_, _, _, _, _, _, _, _, _, _, _, _, _)")
 
 for _,row in dest_df.iterrows():
     prolog.assertz(f"destination('{str(row['Destinations']).lower()}', '{str(row['country']).lower()}', '{str(row['region']).lower()}', '{str(row['Climate']).lower()}', '{str(row['Budget']).lower()}', '{str(row['Activity']).lower()}', '{str(row['Demographics']).lower()}', '{str(row['Duration']).lower()}', '{str(row['Cuisine']).lower()}', '{str(row['History']).lower()}', '{str(row['Natural Wonder']).lower()}', '{str(row['Accommodation']).lower()}', '{str(row['Language']).lower()}')")
+
+
+
 
 
 ################################################################################################
